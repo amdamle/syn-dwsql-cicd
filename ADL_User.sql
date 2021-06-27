703ca68f1cb4d36ea316a14fab5b55b4973252d2
Amit Damle <amdamle@microsoft.com>
2021-06-27 12:19:39 +0530

    updated with devops dynamic param format

--- ADL_User.sql
+++ ADL_User.sql
@@ -1,12 +1,12 @@
 ﻿-- Create a Service Principal and use it for accessing storage
 -- Please make sure to assing Storage Blob Data Contributor access to service principal on your storage
 
-CREATE MASTER KEY ENCRYPTION BY PASSWORD='STRONG_PASSWORD';
+CREATE MASTER KEY ENCRYPTION BY PASSWORD='$(DB_MK_STRONG_PASSWORD)';
 
 GO
 
 CREATE DATABASE SCOPED CREDENTIAL ADL_User
 WITH
-    IDENTITY = 'CLIENT_ID@https://login.microsoftonline.com/TENANT_ID/oauth2/token',
-    SECRET = 'PASSWORD'
+    IDENTITY = '$(CLIENT_ID@https://login.microsoftonline.com/$(TENANT_ID)/oauth2/token',
+    SECRET = '$(APP_PASSWORD)'
 ;
